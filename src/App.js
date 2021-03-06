ab7dd7c50475520c09cb5080f2e32fcb0e0e2313
bagasprakarso <bagas.prakarso@tokopedia.com>
2021-03-07 04:33:41 +0700

    member Home, member Confirm order

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const HomePage = lazy(() => import('./components/HomePage'));
 const Scan = lazy(() => import('./components/Scan/index.js'));
+const MemberLazy = lazy(() => import('./components/Member/index.js'));
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/scan" component={Scan} />
+          <Route path="/scan" component={Scan} />
+          <Route path="/member" component={MemberLazy} />
         </Switch>
       </Suspense>
     </Router>
---
+++ src/components/Member/index.js
@@ -0,0 +1,19 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+const MemberPage = lazy(() => import('./Home/index.js'));
+const MemberOrderSummary = lazy(() => import('./OrderSummary/index.js'));
+
+function Member() {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/member" component={MemberPage} />
+          <Route path="/member/order-summary" component={MemberOrderSummary} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default Member;
