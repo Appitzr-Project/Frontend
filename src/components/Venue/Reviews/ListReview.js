0a328941d658a114e197510c0d0ce408663c0b3b
alfi sahri <alfisahri33@gmail.com>
2021-03-08 10:52:23 +0700

    refactor Venue/Reviews/ListReview.js

--- src/components/Venue/Reviews/ListReview.js
+++ src/components/Venue/Reviews/ListReview.js
@@ -8,7 +8,78 @@ import {
   TextField,
 } from "@material-ui/core";
 import StarIcon from "@material-ui/icons/Star";
-import useStyle from "../../../style";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyle = makeStyles((theme) => ({
+  listReview: {
+    borderRadius: "20px",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.1)",
+    marginTop: "30px",
+  },
+
+  sizeAvatar: {
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+  },
+
+  replyInput: {
+    background: "#f3f3f3",
+    marginTop: "11px",
+    marginBottom: "15px",
+  },
+
+  replyBtn: {
+    background: "#20D3C2",
+    width: "82px",
+    height: "27px",
+    borderRadius: "18px",
+    marginBottom: "16px",
+  },
+
+  leftEl: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  centerEl: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  name: {
+    fontWeight: "700",
+    fontSize: "14px",
+  },
+
+  date: {
+    fontSize: "11px",
+    fontWeight: "400",
+  },
+
+  starWrap: {
+    display: "flex",
+    alignItem: "center",
+    fontSize: "11px",
+  },
+  rating: {
+    marginTop: "4px",
+    marginLeft: "6px",
+    fontWeight: "700",
+  },
+
+  reviewDesc: {
+    fontSize: "11px",
+    fontWeight: "400 ",
+  },
+
+  replyBtn: {
+    fontSize: "14px",
+    fontWeight: "600",
+    marginTop: "15px",
+    float: "right",
+    color: "#5263C8",
+    cursor: "pointer",
+  },
+}));
 
 const ListReview = () => {
   const classes = useStyle();
@@ -17,7 +88,7 @@ const ListReview = () => {
   return (
     <Grid container spacing={2} className={classes.listReview}>
       <Grid item xs={2} onClick={() => setShowReply(false)}>
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className={classes.leftEl}>
           <Avatar
             alt=""
             src="https://source.unsplash.com/random"
@@ -26,36 +97,21 @@ const ListReview = () => {
         </div>
       </Grid>
       <Grid item xs={10}>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography
-            variant="h6"
-            style={{ fontWeight: "700", fontSize: "14px" }}
-          >
+        <div className={classes.centerEl}>
+          <Typography variant="h6" className={classes.name}>
             John Doe
           </Typography>
-          <Typography style={{ fontSize: "11px", fontWeight: "400" }}>
-            4 Days ago
-          </Typography>
+          <Typography className={classes.date}>4 Days ago</Typography>
         </div>
-        <Typography
-          style={{
-            display: "flex",
-            alignItem: "center",
-            fontSize: "11px",
-          }}
-        >
+        <Typography className={classes.starWrap}>
           <StarIcon fontSize="small" htmlColor="#FF7F23" />
           <StarIcon fontSize="small" htmlColor="#FF7F23" />
           <StarIcon fontSize="small" htmlColor="#FF7F23" />
           <StarIcon fontSize="small" htmlColor="#FF7F23" />
-          <span
-            style={{ marginTop: "4px", marginLeft: "6px", fontWeight: "700" }}
-          >
-            4.0
-          </span>
+          <span className={classes.rating}>4.0</span>
         </Typography>
 
-        <Typography style={{ fontSize: "11px", fontWeight: "400 " }}>
+        <Typography className={classes.reviewDesc}>
           This cozy restaurant has left the best impressions! Hospitable hosts,
           delicious dishes, beautiful presentation, wide wine list and wonderful
           dessert.
@@ -77,15 +133,7 @@ const ListReview = () => {
         ) : (
           <Badge
             onClick={() => setShowReply(true)}
-            style={{
-              fontSize: "14px",
-              fontWeight: "600",
-              marginTop: "15px",
-              float: "right",
-              color: "#5263C8",
-              cursor: "pointer",
-            }}
-            color="primary"
+            className={classes.replyBtn}
           >
             Reply
           </Badge>
