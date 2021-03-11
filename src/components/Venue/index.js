20264916bf45e06dec4a09cd1655146d78757694
rahmat <muhamadrahmatsetiawan@gmail.com>
2021-03-11 22:31:33 +0700

    update venue owner index

--- src/components/Venue/index.js
+++ src/components/Venue/index.js
@@ -5,12 +5,7 @@ const MenuOption = lazy(() => import("./MenuOption"));
 const Profile = lazy(() => import("./Profile/Profile"));
 const Reviews = lazy(() => import("./Reviews/Reviews"));
 const Dashboard = lazy(() => import("./Dashboard/Dashboard"));
-<<<<<<< HEAD
-// const Menu = lazy(() => import("./menu"));
-// const MenuAdd = lazy(() => import("./menu/add.js"));
-=======
 const Detail = lazy(() => import("./Detail"));
->>>>>>> f0bf1e4e1c6ad1e0e19ecc9662fffe38475ee0ab
 
 const Venue = () => {
   return (
@@ -21,12 +16,9 @@ const Venue = () => {
           <Route path="/venue/profile" component={Profile} />
           <Route path="/venue/reviews" component={Reviews} />
           <Route path="/venue/dashboard" component={Dashboard} />
-<<<<<<< HEAD
           <Route exact path="/venue/menu" component={View} />
           <Route path="/venue/menu/add" component={Add} />
-=======
           <Route path="/venue/detail" component={Detail} />
->>>>>>> f0bf1e4e1c6ad1e0e19ecc9662fffe38475ee0ab
         </Switch>
       </Suspense>
     </Router>
