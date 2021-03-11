55e77f35bc0dee54cc817dc4e0d0d6d033874668
Bagas Ghufron Alfaiz <bagasghufronalfaiz@users.noreply.github.com>
2021-03-11 14:21:32 +0700

    Update Notification (#16)
    * Update Notification
    
    - convert class component to functional(stateless) component
    - style use material-ui
    - change header into header from shared folder
    - fix icon
    - fix text
    
    * Update Notification
    
    Remove unnecessary style from notification
    
    * Update Chart.js

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const MemberPage = lazy(() => import('./Home/index.js'));
 const MemberOrderSummary = lazy(() => import('./OrderSummary/index.js'));
+const Notification = lazy(() => import('./Notification/Notification'));
 
 function Member() {
   return (
@@ -10,6 +11,7 @@ function Member() {
         <Switch>
           <Route exact path="/member" component={MemberPage} />
           <Route path="/member/order-summary" component={MemberOrderSummary} />
+          <Route path="/member/notification" component={Notification} />
         </Switch>
       </Suspense>
     </Router>
