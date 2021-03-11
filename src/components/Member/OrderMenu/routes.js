025734d12c4a25f28b11024a630808818aaf5fd5
rahmat <muhamadrahmatsetiawan@gmail.com>
2021-03-11 16:45:19 +0700

    update view menu

---
+++ src/components/Member/OrderMenu/routes.js
@@ -0,0 +1,3 @@
+import { lazy } from 'react'
+
+export const MemberOrderMenu = lazy(() => import('./index'))
--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MemberOrderMenu } from './OrderMenu/routes';
 const MemberPage = lazy(() => import('./Home/index.js'));
 const MemberOrderSummary = lazy(() => import('./OrderSummary/index.js'));
 
@@ -9,6 +10,7 @@ function Member() {
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/member" component={MemberPage} />
+          <Route exact path="/member/order-menu" component={MemberOrderMenu} />
           <Route path="/member/order-summary" component={MemberOrderSummary} />
         </Switch>
       </Suspense>
