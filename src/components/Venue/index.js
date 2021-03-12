97302843115889f95f22f641e4b0b1c149e2298d
rahmat <muhamadrahmatsetiawan@gmail.com>
2021-03-13 06:05:31 +0700

    update menu list & add

--- src/components/Venue/index.js
+++ src/components/Venue/index.js
@@ -1,11 +1,13 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Add, View } from './menu'
+// import { Add, View } from './menu'
 const MenuOption = lazy(() => import("./MenuOption"));
 const Profile = lazy(() => import("./Profile/Profile"));
 const Reviews = lazy(() => import("./Reviews/Reviews"));
 const Dashboard = lazy(() => import("./Dashboard/Dashboard"));
 const Detail = lazy(() => import("./Detail"));
+const MenuList = lazy(() => import("./menu"));
+const MenuAdd = lazy(() => import("./menu/MenuAdd"));
 
 const Venue = () => {
   return (
@@ -16,8 +18,8 @@ const Venue = () => {
           <Route path="/venue/profile" component={Profile} />
           <Route path="/venue/reviews" component={Reviews} />
           <Route path="/venue/dashboard" component={Dashboard} />
-          <Route exact path="/venue/menu" component={View} />
-          <Route path="/venue/menu/add" component={Add} />
+          <Route exact path="/venue/menu" component={MenuList} />
+          <Route path="/venue/menu/add" component={MenuAdd} />
           <Route path="/venue/detail" component={Detail} />
         </Switch>
       </Suspense>
