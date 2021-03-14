e1623d2b826e2356d0ccdce852d0ea941a52d936
Bagas Ghufron Alfaiz <bagasghufronalfaiz@gmail.com>
2021-03-14 20:26:59 +0700

    Fix Search button on home
    - Fix Search button on home

--- src/components/Member/Home/components/ButtonSearch.js
+++ src/components/Member/Home/components/ButtonSearch.js
@@ -2,20 +2,24 @@ import { Box, Button, makeStyles } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles({
+  btnBox: {
+    textAlign: 'center',
+  },
   btnSendOrder: {
     textTransform: 'none',
     height: '55px',
-    color: '#ffffff',
-    bottom: 56,
+    color: '#ffffff',    
     width: 'calc(100% - 48px)',
-    margin: '5% auto',
-    left: 0,
-    right: 0,
-    position: 'fixed',
+    margin: '5% auto',    
     maxWidth: '568px',
     boxShadow: '0px -5px 5px rgb(119 118 118 / 21%)',
     background: '#20D3C2',
     borderRadius: '18px',
+    // Uncomment to make it float again
+    // left: 0,
+    // right: 0,
+    // position: 'fixed',
+    // bottom: 56,
   },
 });
 
@@ -23,7 +27,7 @@ const ButtonSearch = () => {
   const classes = useStyles();
 
   return (
-    <Box pl="24px" pr="24px">
+    <Box pl="24px" pr="24px"  className={classes.btnBox}>
       <Button variant="contained" fullWidth className={classes.btnSendOrder}>
         <Box fontWeight={600} fontSize={20}>
           Search
