6fb67b66a6c80efdb90a12d6e9999e5330c53709
alfi sahri <alfisahri33@gmail.com>
2021-03-15 19:48:36 +0700

    add orderDetail route in member/index

---
+++ src/components/Member/OrderDetail/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from "@material-ui/core"
+
+const MemberOrderDetail = () => {
+  return (
+    <>
+      Order Detail
+    </>
+  )
+}
+
+export default MemberOrderDetail;
