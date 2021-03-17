7114ea65dd674102d82633831cfb8beda8a245a6
bagasprakarso <bagas.prakarso@tokopedia.com>
2021-03-17 21:07:57 +0700

    order menu

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const MemberPage = lazy(() => import('./Home/index.js'));
 const ProfilePage = lazy(() => import('./Profile/index.js'));
 const MemberOrderSummary = lazy(() => import('./OrderSummary/index.js'));
-const OrderHistory=lazy(() => import('./OrderHistory/OrderHistory.jsx'));
+const OrderHistory = lazy(() => import('./OrderHistory/OrderHistory.jsx'));
 const Notification = lazy(() => import('./Notification/Notification'));
 const ProfileEdit = lazy(() => import('../ProfileEdit/index'));
 const Support = lazy(() => import('../Support/index'));
 const OrderThankYou = lazy(() => import('./OrderThankYou/OrderThankYou'));
 const Review = lazy(() => import('./Review/Review'));
-const OrderDetail = lazy(()=> import("./OrderDetail"));
+const OrderDetail = lazy(() => import('./OrderDetail'));
+const MemberOrderMenu = lazy(() => import('./OrderMenu'));
 
 function Member() {
   return (
@@ -26,6 +27,7 @@ function Member() {
           <Route exact path="/member/contact/support" component={Support} />
           <Route path="/member/thank-you" component={OrderThankYou} />
           <Route path="/member/review" component={Review} />
+          <Route exact path="/member/order-menu" component={MemberOrderMenu} />
         </Switch>
       </Suspense>
     </Router>
