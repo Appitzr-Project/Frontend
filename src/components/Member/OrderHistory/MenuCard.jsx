d03dbee9dba836deab1b8cb5179a46b6d8722502
franky19 <frankimarbun@gmail.com>
2021-03-20 00:05:55 +0700

    Update Menu list screen

--- src/components/Member/OrderHistory/MenuCard.jsx
+++ src/components/Member/OrderHistory/MenuCard.jsx
@@ -2,7 +2,6 @@ import { Box, Button, Card, Grid, IconButton, makeStyles, Typography } from '@ma
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import React from 'react';
 import { useHistory } from 'react-router';
-
 const useStyles=makeStyles((theme)=>({
     cardContainer: {
         boxShadow: '0px 4px 4px rgba(71, 71, 71, 0.1)',
@@ -16,233 +15,163 @@ const useStyles=makeStyles((theme)=>({
         height: '100%',
     },
     esKrim :{
-        height:100,
-        width:100,
         backgroundImage:`url(${process.env.PUBLIC_URL+'/assets/eskrim.png'})`,
         background: 'no-repeat',
     },
     Spageti :{
-        height:100,
-        width:100,
         backgroundImage:`url(${process.env.PUBLIC_URL+'/assets/spageti.png'})`,
         background: 'no-repeat',
     },
-    DateOrder:{
-        marginTop:'15px',
-        marginBottom:'15px'
-    }
 }))
 
 const MenuCard= () => {
   const classes = useStyles();
-  const history = useHistory()
+  const history = useHistory();
+
   return (
     <>
-        <Grid container  spacing={0} direction="column">
-        <Typography variant="caption" display="block" className={classes.DateOrder}>February 28, 2020</Typography>
-            <Grid item className={classes.gridWrapper}>
-           
-                <Card elevation={0} className={classes.cardContainer}>
-                
-                    <Grid container className={classes.gridContainer}>
-                        <Grid item className={classes.Spageti}>
-                            <IconButton>
-                                <FavoriteIcon style={{ color: 'white' }} />
-                            </IconButton>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <Box ml={3} fontSize={14} color="#7B00AB">
-                                Restaurant Hubert
-                                </Box>
-                                <Box ml={3} fontWeight={600} fontSize={16} color="#0E0E0E">
-                                Medium Spicy Spaghetti
-                                </Box>
-                                <Box
-                                ml={3}
-                                mt={2}
-                                fontWeight={600}
-                                fontSize={16}
-                                color="#5E4E63"
-                                >
-                                $43
-                                </Box>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                style={{color:'#FFCC47'}}
-                                >
-                                waiting payment
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Card>
-                <Card elevation={0} className={classes.cardContainer}>
-                    <Grid container className={classes.gridContainer}>
-                    <Grid item className={classes.esKrim}>
-                            <IconButton>
-                                <FavoriteIcon style={{ color: 'white' }} />
-                            </IconButton>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <Box ml={3} fontSize={14} color="#7B00AB">
-                                Ragazzi WIne and Pasta
-                                </Box>
-                                <Box ml={3} fontWeight={600} fontSize={16} color="#0E0E0E">
-                                Ice Cream Cone
-                                </Box>
-                                <Box
-                                ml={3}
-                                mt={2}
-                                fontWeight={600}
-                                fontSize={16}
-                                color="#5E4E63"
-                                >
-                                $15
-                                </Box>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                style={{color:'#F43939'}}
-                                >
-                                Canceled
-                                </Grid>
-                            </Grid>
-                    </Grid>
-                </Card>
-                <Typography variant="caption" display="block" gutterBottom className={classes.DateOrder}>February 25, 2020</Typography>
-                <Card elevation={0} className={classes.cardContainer}>
-                    <Grid container className={classes.gridContainer}>
-                            <Grid item className={classes.Spageti}>
-                            <IconButton>
-                                <FavoriteIcon style={{ color: 'white' }} />
-                            </IconButton>
-                            </Grid>
-                            <Grid item xs={8} >
-                                <Box ml={3} fontSize={14} color="#7B00AB">
-                                Restaurant Hubert
-                                </Box>
-                                <Box ml={3} fontWeight={600} fontSize={16} color="#0E0E0E">
-                                Medium Spicy Spaghetti
-                                </Box>
-                                <Box
-                                ml={3}
-                                mt={2}
-                                fontWeight={600}
-                                fontSize={16}
-                                color="#5E4E63"
-                                >
-                                $43
-                                </Box>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                style={{color:'#11B151'}}
-                                >
-                                Completed
-                                </Grid>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                >
-                                <Button variant="contained" color="primary" onClick={()=>history.push('/member/review')}style={{borderRadius:'50px 50px 50px 50px',fontSize:'12px',marginTop:'2px', background: '#20D3C2'}}>
-                                    add review
-                                </Button>
-                                </Grid>
-                            </Grid>
-                    </Grid>
-                </Card>
-                <Typography variant="caption" display="block" className={classes.DateOrder}>February 23, 2020</Typography>
-                <Card elevation={0} className={classes.cardContainer}>
-                
-                    <Grid container className={classes.gridContainer}>
-                        <Grid item className={classes.Spageti}>
-                            <IconButton>
-                                <FavoriteIcon style={{ color: 'white' }} />
-                            </IconButton>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <Box ml={3} fontSize={14} color="#7B00AB">
-                                Restaurant Hubert
-                                </Box>
-                                <Box ml={3} fontWeight={600} fontSize={16} color="#0E0E0E">
-                                Medium Spicy Spaghetti
-                                </Box>
-                                <Box
-                                ml={3}
-                                mt={2}
-                                fontWeight={600}
-                                fontSize={16}
-                                color="#5E4E63"
-                                >
-                                $43
-                                </Box>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                style={{color:'#11B151'}}
-                                >
-                                Completed
-                                </Grid>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                >
-                                <Button variant="contained" color="primary" onClick={()=>history.push('/member/review')} style={{borderRadius:'50px 50px 50px 50px',fontSize:'12px',marginTop:'2px', background: '#20D3C2'}}>
-                                    add review
-                                </Button>
-                                </Grid>
-                        </Grid>
-                    </Grid>
-                </Card>
-                <Card elevation={0} className={classes.cardContainer}>
-                    <Grid container className={classes.gridContainer}>
-                    <Grid item className={classes.esKrim}>
-                            <IconButton>
-                                <FavoriteIcon style={{ color: 'white' }} />
-                            </IconButton>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <Box ml={3} fontSize={14} color="#7B00AB">
-                                Ragazzi WIne and Pasta
-                                </Box>
-                                <Box ml={3} fontWeight={600} fontSize={16} color="#0E0E0E">
-                                Ice Cream Cone
-                                </Box>
-                                <Box
-                                ml={3}
-                                mt={2}
-                                fontWeight={600}
-                                fontSize={16}
-                                color="#5E4E63"
-                                >
-                                $15
-                                </Box>
-                                <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                style={{color:'#F43939'}}
-                                >
-                                Canceled
-                                </Grid>
-                            </Grid>
-                    </Grid>
-                </Card>
+    <Grid
+        container spacing={0} direction="column"
+    >
+        {Array.from({ length: 5 }).map((i, index) => (
+        <Grid item className={classes.gridWrapper}>
+             <Typography>28 February 2021</Typography>
+          <Card elevation={0} className={classes.cardContainer}>
+            <Grid container className={classes.gridContainer}>
+              <Grid item xs={3} className={classes.Spageti}>
+                <Box p={1}>
+                <IconButton>
+                    <FavoriteIcon style={{ color: 'white' }} />
+                </IconButton>
+                </Box>
+              </Grid>
+              <Grid item xs={8}>
+                <Box ml={3} fontSize={14} fontFamily='Open Sans' color="#7B00AB">
+                Restaurant Hubert
+                </Box>
+                <Box ml={3} fontWeight={600} fontFamily='Open Sans' fontSize={16} color="#0E0E0E">
+                  Medium Spicy Spaghetti
+                </Box>
+                <Box
+                  ml={3}
+                  mt={2}
+                  fontWeight={600}
+                  fontSize={16}
+                  color="#5E4E63"
+                  fontFamily='Open Sans'
+                >
+                  $43
+                </Box>
+              </Grid>
             </Grid>
+            <Grid
+              container
+              justify="flex-end"
+              className={classes.gridContainer}
+            >
+              <Grid item>
+                <Box fontWeight={700} fontStyle={16} mb={2} ml={3} mr={3} style={{color:'#FFCC47'}}>
+                  waiting payment
+                </Box>
+              </Grid>
+            </Grid>
+          </Card>
+          <Card elevation={0} className={classes.cardContainer}>
+            <Grid container className={classes.gridContainer}>
+              <Grid item xs={3} className={classes.Spageti}>
+                <Box p={1}>
+                <IconButton>
+                    <FavoriteIcon style={{ color: 'white' }} />
+                </IconButton>
+                </Box>
+              </Grid>
+              <Grid item xs={8}>
+                <Box ml={3} fontSize={14} fontFamily='Open Sans' color="#7B00AB">
+                Restaurant Hubert
+                </Box>
+                <Box ml={3} fontWeight={600} fontFamily='Open Sans' fontSize={16} color="#0E0E0E">
+                  Medium Spicy Spaghetti
+                </Box>
+                <Box
+                  ml={3}
+                  mt={2}
+                  fontWeight={600}
+                  fontSize={16}
+                  color="#5E4E63"
+                  fontFamily='Open Sans'
+                >
+                  $43
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              justify="flex-end"
+              className={classes.gridContainer}
+            >
+              <Grid item>
+                <Box fontWeight={700} fontStyle={16} mb={2} ml={3} mr={3} style={{color:'#F43939'}}>
+                  canceled
+                </Box>
+              </Grid>
+            </Grid>
+          </Card>
+          <Card elevation={0} className={classes.cardContainer}>
+            <Grid container className={classes.gridContainer}>
+              <Grid item xs={3} className={classes.Spageti}>
+                <Box p={1}>
+                <IconButton>
+                    <FavoriteIcon style={{ color: 'white' }} />
+                </IconButton>
+                </Box>
+              </Grid>
+              <Grid item xs={8}>
+                <Box ml={3} fontSize={14} fontFamily='Open Sans' color="#7B00AB">
+                Restaurant Hubert
+                </Box>
+                <Box ml={3} fontWeight={600} fontFamily='Open Sans' fontSize={16} color="#0E0E0E">
+                  Medium Spicy Spaghetti
+                </Box>
+                <Box
+                  ml={3}
+                  mt={2}
+                  fontWeight={600}
+                  fontSize={16}
+                  color="#5E4E63"
+                  fontFamily='Open Sans'
+                >
+                  $43
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              justify="flex-end"
+              className={classes.gridContainer}
+            >
+              <Grid item>
+                <Box fontWeight={700} fontFamily='Open Sans' fontStyle={16} mb={2} ml={3} mr={3} style={{color:'#11B151'}}>
+                  completed
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              justify="flex-end"
+              className={classes.gridContainer}
+            >
+              <Grid item>
+                <Box fontWeight={700} fontFamily='Open Sans' fontStyle={16} mb={2} ml={3} mr={3} style={{color:'#FFCC47'}}>
+                <Button variant="contained" color="primary" onClick={()=>history.push('/member/review')}style={{borderRadius:'50px 50px 50px 50px',fontSize:'12px',marginTop:'2px', background: '#20D3C2'}}>
+                    add review
+                </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Card>
         </Grid>
+      ))}
+        </Grid> 
     </>
   );
 };
