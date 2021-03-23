0b041b53caaa61d4453a6237c9598bddee7cc297
andrel <andrel.sitanggang@gmail.com>
2021-03-23 21:25:34 +0700

    feat: Add upload new menu

--- src/components/Venue/MenuForm/index.js
+++ src/components/Venue/MenuForm/index.js
@@ -1,87 +1,137 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { Typography, Grid, Button } from '@material-ui/core'
-import Wrapper from '../../shared/Wrapper'
-import { makeStyles } from '@material-ui/core/styles'
-import { strings } from './utils'
-import { InputForm, SelectOption } from './components'
-import { Link } from 'react-router-dom'
-import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary'
+import React, { useRef, useEffect, useState } from "react";
+import { Typography, Grid, Button } from "@material-ui/core";
+import Wrapper from "../../shared/Wrapper";
+import { makeStyles } from "@material-ui/core/styles";
+import { strings } from "./utils";
+import { InputForm, SelectOption } from "./components";
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const MenuAdd = () => {
-  const [state, setState] = useState({
-    name: '',
-    description: '',
+  const auth = useSelector((state) => state.auth);
+  let history = useHistory();
+
+  const rawData = {
+    name: "",
+    description: "",
     category: [
       {
         id: 1,
-        name: 'Dessert'
+        name: "Dessert",
       },
       {
         id: 2,
-        name: 'Sphagetti'
+        name: "Sphagetti",
       },
       {
         id: 3,
-        name: 'Drink'
-      }
+        name: "Drink",
+      },
     ],
-    price: '',
-    picture: null,
-    protein: [
+    price: "",
+    picture: [],
+    proteinType: [
       {
         id: 1,
-        name: 'Vegan'
+        name: "Vegan",
       },
       {
         id: 2,
-        name: 'Meat'
+        name: "Meat",
       },
       {
         id: 3,
-        name: 'Vegetables'
-      }
-    ]
-  })
-  const classes = useStyle()
-  const [picturePreview, setPicturePriview] = useState(null)
-  const inputFocus = useRef(null)
+        name: "Vegetables",
+      },
+    ],
+  };
+
+  const [state, setState] = useState({
+    productName: "",
+    description: "",
+    category: "",
+    price: 0,
+    images: [],
+    proteinType: "",
+  });
+
+  const [picturePreview, setPicturePreview] = useState([]);
+  const classes = useStyle();
+  const inputFocus = useRef(null);
 
   useEffect(() => {
     if (inputFocus.current) {
-      inputFocus.current.focus()
+      inputFocus.current.focus();
     }
-  }, [])
+  }, []);
 
-  const onChange = event => {
-    const { name, value } = event.target
-    setState(prevState => ({
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevState) => ({
       ...prevState,
-      [name]: value
-    }))
-  }
+      [name]: name === "price" ? parseInt(value) : value,
+    }));
+  };
 
-  const _handleChangePicture = (event) => {
-    event.preventDefault()
-    setState(prevState => ({
+  const _handleChangePicture = async (event) => {
+    event.persist();
+
+    const file = event.target.files[0];
+
+    let formData = new FormData();
+    formData.append("data", file);
+
+    const rawResponse = await fetch(
+      "https://api.dev.appetizr.co/products/venue/upload",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${auth.user.signInUserSession.idToken.jwtToken}`,
+        },
+        body: formData,
+      }
+    );
+    const result = await rawResponse.json();
+
+    setState((prevState) => ({
       ...prevState,
-      picture: event.target.files[0]
-    }))
-    setPicturePriview(URL.createObjectURL(event.target.files[0]))
-  }
+      images: picturePreview.concat(result.data),
+    }));
+
+    setPicturePreview((prevState) => prevState.concat(result.data));
+  };
+
+  const handleSaveMenu = async () => {
+    const rawResponse = await fetch(
+      "https://api.dev.appetizr.co/products/venue",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.user.signInUserSession.idToken.jwtToken}`,
+        },
+        body: JSON.stringify(state),
+      }
+    );
+    const result = await rawResponse.json();
+    if (result.code === 200 && result.message === "success") {
+      history.push("/venue/menu/list");
+    }
+  };
 
   return (
     <Wrapper
       title={strings.add_menu}
-      image='https://source.unsplash.com/random'
-      spacing='26px'
-      isBack='/venue/menu/list'
+      image="https://source.unsplash.com/random"
+      spacing="26px"
+      isBack="/venue/menu/list"
     >
       <form>
-        <Grid container direction='column'>
+        <Grid container direction="column">
           <Grid item xs={12}>
             <InputForm
-              type='text'
-              name='name'
+              type="text"
+              name="productName"
               value={state.name}
               label={strings.label_name}
               ref={inputFocus}
@@ -93,190 +143,179 @@ const MenuAdd = () => {
           <SelectOption
             label={strings.label_category}
             className={classes.label}
-            data={state.category}
+            data={rawData.category}
+            inputName="category"
+            onHandleChange={(e) => onChange(e)}
           />
         </Grid>
         <Grid item xs={12}>
-          <Typography
-            gutterBottom
-            className={classes.label}>
+          <Typography gutterBottom className={classes.label}>
             {strings.label_desc}
           </Typography>
           <textarea
-            name='description'
-            rows='2' type='text'
+            name="description"
+            rows="2"
+            type="text"
             className={classes.input}
+            onChange={onChange}
             ref={inputFocus}
           />
         </Grid>
         <Grid item xs={12}>
           <InputForm
-            type='text'
-            name='price'
+            type="text"
+            name="price"
             value={state.price}
-            label='Price'
+            label="Price"
             ref={inputFocus}
             onChange={onChange}
           />
         </Grid>
         <Grid item xs={12}>
-          <Typography
-            gutterBottom
-            className={classes.label}>
+          <Typography gutterBottom className={classes.label}>
             {strings.label_picture}
           </Typography>
-          {picturePreview === null && (
-            <Button
+          {!picturePreview.length && (
+            <input
+              type="file"
+              name="picture"
               onChange={_handleChangePicture}
-              variant='contained'
-              component='label'
-              startIcon={<PhotoLibraryIcon style={{ fontSize: 100, color: '#979797' }} />}
-              className={classes.btnPreview}
-              disableElevation
-            >
-              <input
-                type='file'
-                name={state.picture}
-                hidden
-              />
-            </Button>
+              className={classes.inputFile}
+            />
           )}
         </Grid>
-        {picturePreview !== null && (
+        {picturePreview.length ? (
           <>
             <div className={classes.formGroup}>
-              <img
-                src={picturePreview}
-                alt='img-product'
-                className={classes.imgPreview}
-              />
+              {picturePreview.map((val) => (
+                <img
+                  src={val}
+                  alt="img-product"
+                  className={classes.imgPreview}
+                />
+              ))}
             </div>
             <div className={classes.formGroup}>
-              <Button
-                onChange={_handleChangePicture}
-                variant='contained'
-                component='label'
-                className={classes.btnImg}
-                onChange={_handleChangePicture}
-              >
+              <label for="upload-photo" className={classes.btnImg}>
                 {strings.add_img}
-                <input
-                  type='file'
-                  name={state.picture}
-                  hidden
-                />
-              </Button>
+              </label>
+              <input
+                type="file"
+                id="upload-photo"
+                name="picture"
+                onChange={_handleChangePicture}
+                style={{
+                  opacity: 0,
+                  position: "absolute",
+                  zIndex: -1,
+                }}
+              />
             </div>
           </>
-        )}
-        <Grid item xs={12} style={{ margin: '10px 0px 54px 0px' }}>
+        ) : null}
+        <Grid item xs={12} style={{ margin: "10px 0px 54px 0px" }}>
           <SelectOption
             label={strings.label_protein_type}
             className={classes.label}
-            data={state.protein}
+            data={rawData.proteinType}
+            inputName="proteinType"
+            onHandleChange={(e) => onChange(e)}
           />
         </Grid>
         <Grid item xs={12}>
-          <Link to='/venue/menu/list'>
-            <Button className={classes.submitBtn} fullWidth>
-              <Typography
-                variant='subtitle2'
-                className={classes.bold}
-                style={{ color: 'white', textTransform: 'capitalize' }}
-              >
-                {strings.save_menu}
-              </Typography>
-            </Button>
-          </Link>
+          <Button
+            className={classes.submitBtn}
+            fullWidth
+            onClick={handleSaveMenu}
+          >
+            <Typography
+              variant="subtitle2"
+              className={classes.bold}
+              style={{ color: "white", textTransform: "capitalize" }}
+            >
+              {strings.save_menu}
+            </Typography>
+          </Button>
         </Grid>
       </form>
     </Wrapper>
-  )
-}
+  );
+};
 
 const useStyle = makeStyles((theme) => ({
   bold: {
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   formGroup: {
-    position: 'relative',
-    marginBottom: '16px',
-    width: '100%'
+    position: "relative",
+    marginBottom: "16px",
+    width: "100%",
   },
   submitBtn: {
-    backgroundColor: '#20D3C2',
-    borderRadius: '12px',
-    height: '48px',
-    padding: '16px',
-    boxShadow: '1px 1px 12px -3px',
-    '&:hover': {
-      backgroundColor: '#20D3C2'
-    }
+    backgroundColor: "#20D3C2",
+    borderRadius: "12px",
+    height: "48px",
+    padding: "16px",
+    boxShadow: "1px 1px 12px -3px",
+    "&:hover": {
+      backgroundColor: "#20D3C2",
+    },
   },
   input: {
-    background: '#f3f3f3',
-    borderRadius: '10px',
-    width: '100%',
+    background: "#f3f3f3",
+    borderRadius: "10px",
+    width: "100%",
     border: 0,
     outline: 0,
-    boxShadow: 'none',
-    padding: '12px',
-    '&:focus': {
-      border: '2px solid #3f51b5'
-    }
+    boxShadow: "none",
+    padding: "12px",
+    "&:focus": {
+      border: "2px solid #3f51b5",
+    },
   },
   select: {
-    backgroundColor: '#f3f3f3',
-    width: '100%',
-    position: 'relative',
-    borderRadius: '10px',
-    padding: '16px',
-    border: 'none',
-    [theme.breakpoints.down('xs')]: {
-      padding: '14px'
+    backgroundColor: "#f3f3f3",
+    width: "100%",
+    position: "relative",
+    borderRadius: "10px",
+    padding: "16px",
+    border: "none",
+    [theme.breakpoints.down("xs")]: {
+      padding: "14px",
     },
-    '&:focus': {
-      border: '2px solid #3f51b5'
-    }
+    "&:focus": {
+      border: "2px solid #3f51b5",
+    },
   },
   imgPreview: {
-    width: '100%',
-    height: '200px',
-    objectFit: 'cover',
-    objectPosition: '50% 50%',
-    borderRadius: '10px',
-    transition: 'opacity .3s'
+    width: "100%",
+    height: "200px",
+    objectFit: "contain",
+    objectPosition: "50% 50%",
+    borderRadius: "10px",
+    transition: "opacity .3s",
+    marginBottom: 15,
   },
   inputFile: {
-    width: '100%',
-    height: '100px',
-    backgroundColor: '#f3f3f3',
-    color: '#000',
-    border: '1px solid #f3f3f3',
-    padding: '16px',
-    borderRadius: '10px',
-    '&:focus': {
-      border: '2px solid #3f51b5'
-    }
-  },
-  btnPreview: {
-    width: '100%',
-    height: '200px',
-    background: '#f3f3f3',
-    borderRadius: '10px',
-    boxShadow: 'none',
-    '&:focus': {
-      border: '2px solid #3f51b5'
-    }
+    width: "100%",
+    height: "100px",
+    backgroundColor: "#f3f3f3",
+    color: "#000",
+    border: "1px solid #f3f3f3",
+    padding: "16px",
+    borderRadius: "10px",
+    "&:focus": {
+      border: "2px solid #3f51b5",
+    },
   },
   btnImg: {
-    border: 'none',
-    padding: '10px',
+    border: "none",
+    padding: "10px",
     fontSize: 14,
-    borderRadius: '15px',
-    backgroundColor: '#4F147B',
-    color: 'white'
-  }
-}))
+    borderRadius: "15px",
+    backgroundColor: "#4F147B",
+    color: "white",
+  },
+}));
 
-export default MenuAdd
+export default MenuAdd;
