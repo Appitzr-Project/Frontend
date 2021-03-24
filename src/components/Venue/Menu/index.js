efe5218021c690c01606e0bc0a91b4891c8481d9
alfi sahri <alfisahri33@gmail.com>
2021-03-24 13:46:05 +0700

    integrate to API (#69)
    * integrate to API
    
    * add loading
    
    * add products.api
    
    * add tryCatch
    
    * delete .env

--- src/components/Venue/Menu/index.js
+++ src/components/Venue/Menu/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Card, Container, makeStyles } from '@material-ui/core'
 import NavBar from '../../NavBar'
 import { IMSpaghetti } from './assets'
 import { SendButton, MenuCard } from './components'
+import { useSelector }  from "react-redux"
+import {productsVenueList} from "../../../redux/api/products.api"
 
 const useStyles = (img) =>
   makeStyles({
@@ -12,12 +14,38 @@ const useStyles = (img) =>
     cardRoot: {
       padding: '24px 24px 140px 24px',
       marginTop: '24px',
-      borderRadius: '50px 50px 0 0'
+      borderRadius: '50px 50px 0 0',
+      minHeight: "100vh"
     }
   })
 
 const MenuList = () => {
   const classes = useStyles(IMSpaghetti)()
+  const auth = useSelector(state => state.auth)
+  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    let isActive = true
+    const http = async () => {
+      try {
+        setLoading(true)
+        const res = await productsVenueList(auth.user.signInUserSession.idToken.jwtToken)
+        if(isActive){
+          setItems(res.data)
+          setLoading(false)
+        }
+      } catch (error) {
+        console.log(error)
+        console.log(error.response)
+      }
+    }
+    http()
+    return () => {
+      isActive=false;
+    }
+  },[auth.user.signInUserSession.idToken.jwtToken])
+
   return (
     <Container
       classes={{ root: classes.containerRoot }}
@@ -26,7 +54,7 @@ const MenuList = () => {
     >
       <NavBar title='Menu' />
       <Card classes={{ root: classes.cardRoot }} elevation={0}>
-        <MenuCard />
+        {loading ? "Loading..." : <MenuCard items={items} />}
       </Card>
       <SendButton />
     </Container>
