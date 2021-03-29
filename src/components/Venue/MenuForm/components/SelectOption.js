d448b562083421267c1c09965f1494b67229c9bd
JuniYadi <juniyadi94@gmail.com>
2021-03-30 04:49:44 +0700

    Update SelecOption With capitalizeFirstLetter

--- src/components/Venue/MenuForm/components/SelectOption.js
+++ src/components/Venue/MenuForm/components/SelectOption.js
@@ -7,6 +7,7 @@ import {
   MenuItem,
   Typography,
 } from "@material-ui/core";
+import { capitalizeFirstLetter } from "../../../../helpers/helper";
 
 const SelectInput = withStyles((theme) => ({
   input: {
@@ -43,7 +44,7 @@ const SelecOption = (props) => {
         >
           {data.map((item) => (
             <MenuItem key={item.id} value={item.name}>
-              {item.name}
+              {capitalizeFirstLetter(item.name)}
             </MenuItem>
           ))}
         </Select>
