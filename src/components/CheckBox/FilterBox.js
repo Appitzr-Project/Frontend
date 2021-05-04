1e5848973656121821ade984d8773aaf4aea6bc1
franky19 <frankimarbun@gmail.com>
2021-05-04 21:42:01 +0700

    add responsive side

--- src/components/CheckBox/FilterBox.js
+++ src/components/CheckBox/FilterBox.js
@@ -20,15 +20,20 @@ export default function FilterBox() {
   </div>
 }
   return (
-    <div className="container">
-      <div className="col-sm-6 mt-2">
-        <h4>
-      <button className='btn px-2 py-1 ' onClick={() => toggleDisplayCategories(!isDispalyCategories)} >Categories
-        {isDispalyCategories ? <MdKeyboardArrowUp /> :  <MdKeyboardArrowDown />} 
-      </button>
-      </h4>
-      </div>
-    </div>
-
+    <div className="container margin_30_20">			
+			<div className="row">
+				<aside className="col-lg-3" id="sidebar_fixed">
+						{/* <!-- /filter_type --> */}
+						<div className="filter_type">
+            <h4>
+            <button className='btn px-2 py-1 ' onClick={() => toggleDisplayCategories(!isDispalyCategories)} >Categories
+              {isDispalyCategories ? <MdKeyboardArrowUp /> :  <MdKeyboardArrowDown />} 
+            </button>
+            </h4>
+						</div>
+				</aside>
+						</div>
+					</div>
+  
   );
 }
