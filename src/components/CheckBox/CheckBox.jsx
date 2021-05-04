1f9a5b88f2880a7df596909efd5fc610a9f975fe
franky19 <frankimarbun@gmail.com>
2021-05-04 16:04:00 +0700

    convert categories to bootrap

--- src/components/CheckBox/CheckBox.jsx
+++ src/components/CheckBox/CheckBox.jsx
@@ -1,29 +1,34 @@
 import { Checkbox, FormControlLabel, makeStyles } from "@material-ui/core"
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '&:hover': {
-            color: "#D50000",
-         },
-    },
-  }));
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         '&:hover': {
+//             color: "#D50000",
+//          },
+//     },
+//   }));
 
 const CheckBox = (props) => {
-    const classes = useStyles();
+    // const classes = useStyles();
     return (
-        <div>
-           <FormControlLabel
-        control={
-          <Checkbox
-            name={props.name}
-            color="secondary"
-            className={classes.root}
-          />
-        }
-        label={props.label}
-       
-      />
-        </div>
+        <div className='col-sm-9 d-flex flex-wrap ' >
+      <div className="form-check mr-2" >
+            <input
+              className="form-check-input"
+              type="Checkbox"
+              name={props.name}
+              id={props.id}
+              />
+          <label className="form-check-label" htmlFor={props.id}>
+             {props.title}
+          </label>
+      </div>
+      <div className="form-check mr-7" >
+          <label className="form-check-label" htmlFor={props.id}>
+            <small>{props.qty}</small>
+          </label>
+    </div>
+    </div>
     )
 }
 export default CheckBox
--- src/components/CheckBox/FilterBox.js
+++ src/components/CheckBox/FilterBox.js
@@ -1,68 +1,34 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {Card,CardContent,CardActions,Collapse,Typography,IconButton} from "@material-ui/core"
-import clsx from 'clsx';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md'
+import { useState } from 'react';
 import CheckBox from './CheckBox'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-    
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-    color: 'black'
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  checkboxId:{
-      marginLeft:'1rem'
-  },
-  labelText:{
-    color: 'black'
-  }
-}));
-
-export default function RecipeReviewCard() {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
-    <Card className={classes.root}>
-      <CardActions disableSpacing>
-        <IconButton aria-label="categories" 
-          onClick={handleExpandClick}
-          aria-expanded={expanded}>
-            <Typography variant='h4' fontWeight={14} className={classes.labelText}>Categories</Typography>
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded}>
-        <CardContent>
-        {Array.from({ length: 5 }).map((i, index) => (
-            <CheckBox label='Pizza' className={classes.checkboxId}/>
+export default function FilterBox() {
+  const [isDispalyCategories, toggleDisplayCategories ] = useState  (false)
+  if(isDispalyCategories){
+    return  <div className="container">
+    <div className="col-sm-6 mt-2">
+      <h4>
+    <button className='btn px-2 py-1 ' onClick={() => toggleDisplayCategories(!isDispalyCategories)} >Categories
+      {isDispalyCategories ? <MdKeyboardArrowUp /> :  <MdKeyboardArrowDown />} 
+    </button>
+    </h4>
+    {Array.from({ length: 5 }).map((i, index) => (
+     <CheckBox title="Pizza" qty={12} id="category"/>
       ))}
-        </CardContent>
-      </Collapse>
-    </Card>
+    
+    </div>
+  </div>
+}
+  return (
+    <div className="container">
+      <div className="col-sm-6 mt-2">
+        <h4>
+      <button className='btn px-2 py-1 ' onClick={() => toggleDisplayCategories(!isDispalyCategories)} >Categories
+        {isDispalyCategories ? <MdKeyboardArrowUp /> :  <MdKeyboardArrowDown />} 
+      </button>
+      </h4>
+      </div>
+    </div>
+
   );
 }
