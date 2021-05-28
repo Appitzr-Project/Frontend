96b47fe9cc58cd8109e7b41ac940f184402df244
JuniYadi <juniyadi94@gmail.com>
2021-05-28 16:59:37 +0700

    Update `src/index.js` - Allow Amplify Login to Google From Local or Production

--- src/index.js
+++ src/index.js
@@ -15,19 +15,40 @@ import { persistStore } from 'redux-persist';
 import Amplify from 'aws-amplify';
 import awsExports from './aws-exports';
 
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === "[::1]" ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
 
+const oauthRedirect = awsExports.oauth.redirectSignIn.split(",");
+const localRedirect = oauthRedirect.filter((val) => val.includes('localhost'))[0];
+const productionRedirect = oauthRedirect.filter((val) => !val.includes('localhost'))[0];
+
+const updatedAwsConfig = {
+  ...awsExports,
+  oauth: {
+    ...awsExports.oauth,
+    redirectSignIn: isLocalhost ? localRedirect : productionRedirect,
+    redirectSignOut: isLocalhost ? localRedirect : productionRedirect,
+  }
+}
 
 const store = createStore(rootReducer, applyMiddleware(thunk, apiMiddleware));
 const persistor = persistStore(store);
 
-Amplify.configure(awsExports);
+Amplify.configure(updatedAwsConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={<div>Loading..</div>}>
         {/* <Authenticator>  */}
-          <App />
+        <App />
         {/* </Authenticator> */}
       </PersistGate>
     </Provider>
