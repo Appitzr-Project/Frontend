7b98e166a5da55cc3fdc0375ed4e4863e8174726
alfi sahri <alfisahri33@gmail.com>
2021-06-13 16:32:39 +0700

    update typo scanqrcode

--- src/routes/VenueList/index.js
+++ src/routes/VenueList/index.js
@@ -88,7 +88,7 @@ const Index = () => {
             <aside className="col-lg-3" id="sidebar_fixed">
               <div className="row">
                 <div className="col-12 mx-auto">
-                  <Link to="/scan" className="btn_1 full-width mb_5">Scan QrCode</Link>
+                  <Link to="/scan" className="btn_1 full-width mb_5">Scan Qr Code</Link>
                 </div>
               </div>
               {/* <a className="btn_map d-flex align-items-center justify-content-center" data-toggle="collapse" href="#collapseMap" aria-expanded="false" aria-controls="collapseMap" ><span className="btn_map_txt" data-text-swap="Hide Map" data-text-original="View on Map">View on Map</span></a> */}
